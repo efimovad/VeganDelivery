0f75f8f26f39c06d053f94a167d673e7fc082522
Yunusov Nozim <nozimdev@gmail.com>
2020-04-26 16:36:01 +0300

    add personal adapter

---
+++ app/src/main/java/com/nozimy/vegandelivery/ui/personal/PersonalAdapter.java
@@ -0,0 +1,59 @@
+package com.nozimy.vegandelivery.ui.personal;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.nozimy.vegandelivery.R;
+
+import java.util.List;
+
+public class PersonalAdapter extends RecyclerView.Adapter<PersonalAdapter.PersonalSettingViewHolder> {
+
+    class PersonalSettingViewHolder extends RecyclerView.ViewHolder {
+        private final TextView settingItemView;
+
+        private PersonalSettingViewHolder(View itemView) {
+            super(itemView);
+            settingItemView = itemView.findViewById(R.id.textView);
+        }
+    }
+
+    private final LayoutInflater mInflater;
+    private List<String> mList;
+
+    PersonalAdapter(Context context) { mInflater = LayoutInflater.from(context); }
+
+    @Override
+    public PersonalSettingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.recyclerview_item_personal, parent, false);
+        return new PersonalSettingViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(PersonalSettingViewHolder holder, int position) {
+        if (mList != null) {
+            String current = mList.get(position);
+            holder.settingItemView.setText(current);
+        } else {
+            holder.settingItemView.setText("No list");
+        }
+    }
+
+    void setWords(List<String> list){
+        mList = list;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mList != null)
+            return mList.size();
+        else return 0;
+    }
+}
--- app/src/main/java/com/nozimy/vegandelivery/ui/personal/PersonalFragment.java
+++ app/src/main/java/com/nozimy/vegandelivery/ui/personal/PersonalFragment.java
@@ -9,48 +9,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.nozimy.vegandelivery.R;
 
-//public class PersonalFragment extends Fragment {
-//
-//    private PersonalViewModel personalViewModel;
-//    private RecyclerView recyclerView;
-//    private PersonalAdapter adapter;
-//
-//    public View onCreateView(@NonNull LayoutInflater inflater,
-//                             ViewGroup container, Bundle savedInstanceState) {
-//        personalViewModel =
-//                ViewModelProviders.of(this).get(PersonalViewModel.class);
-//        View root = inflater.inflate(R.layout.fragment_personal, container, false);
-////        final TextView textView = root.findViewById(R.id.text_notifications);
-////        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-////            @Override
-////            public void onChanged(@Nullable String s) {
-////                textView.setText(s);
-////            }
-////        });
-//
-//        recyclerView = root.findViewById(R.id.personal_list);
-//        adapter = new PersonalAdapter(getContext());
-//        recyclerView.setAdapter(adapter);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-//
-//        personalViewModel.getSettingItems().observe(getViewLifecycleOwner(), new Observer<List<String>>() {
-//            @Override
-//            public void onChanged(List<String> strings) {
-//                adapter.setWords(strings);
-//            }
-//        });
-//
-//        return root;
-//    }
-//}
+import java.util.List;
 
 public class PersonalFragment extends Fragment {
     private PersonalFragmentListener listener;
-    private PersonalViewModel model;
+
+    private PersonalViewModel personalViewModel;
+    private RecyclerView recyclerView;
+    private PersonalAdapter adapter;
 
     public interface PersonalFragmentListener { }
 
@@ -64,10 +37,21 @@ public class PersonalFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        model =
+        personalViewModel =
                 ViewModelProviders.of(this).get(PersonalViewModel.class);
         View root = inflater.inflate(R.layout.fragment_personal, container, false);
-        //final TextView textView = root.findViewById(R.id.text_dashboard);
+
+        recyclerView = root.findViewById(R.id.personal_list);
+        adapter = new PersonalAdapter(getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        personalViewModel.getSettingItems().observe(getViewLifecycleOwner(), new Observer<List<String>>() {
+            @Override
+            public void onChanged(List<String> strings) {
+                adapter.setWords(strings);
+            }
+        });
 
         return root;
     }
